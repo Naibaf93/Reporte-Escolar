e94baa0a5fd7b864118f36a062274184fe8d3c9f
Fabián Huertas <miloh9318@hotmail.com>
2025-03-15 19:27:58 -0500

    agregando al  body  del html un onload para llamar la funcion listarNotas y primeros pasos del loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,10 @@ let array = [7, 4, 2, 9, 6];
 
 function listarNotas() {
     let lista = document.getElementById("listaNotas");
+
+    for(let nota of array){
+        let item = document.createElement("li");
+        item.innerText = nota;
+        lista.appendChild(item);
+    }
 }
